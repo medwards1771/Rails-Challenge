69c0d2442513800abb23ad03ed7450054c77f38a
Meredith Edwards <meredith.edwards1771@gmail.com>
2016-07-05 10:28:34 -0400

    extract logic from views into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def page_name
     @page_name || "#{controller_name} #{action_name}"
   end
+
+  def title(title = nil)
+    if title.present?
+      content_for :title, title
+    else
+      content_for?(:title) ? content_for(:title) : APP_CONFIG['default_title']
+    end
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Load variables from config.yml
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")
